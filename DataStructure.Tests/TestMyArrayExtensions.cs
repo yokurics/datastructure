c26c43df2c160b72052f2b30366f4622d664314a
yokurics <yokurics@gmail.com>
2019-08-27 00:04:20 +0900

    create IterativeBinarySearch, RecursiveBinarySearch

---
+++ DataStructure.Tests/TestMyArrayExtensions.cs
@@ -0,0 +1,37 @@
+﻿using DataStructure.Extentions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DataStructure.Tests
+{
+    [TestClass]
+    public class TestMyArrayExtensions
+    {
+        [TestMethod]
+        public void TestIterativeBinarySearch()
+        {
+            var items = new int[] { 1, 3, 5, 7, 9, 11 };
+            var values = new int[] { 3, 7, 9, 11 };
+            var expecteds = new int[] { 1, 3, 4, 5 };
+            var actuals = new int[4];
+
+            for (var i = 0; i < values.Length; i++)
+            {
+                actuals[i] = items.IterativeBinarySearch(values[i], 0, items.Length);
+            }
+            
+            Assert.AreEqual(expecteds.Length, actuals.Length);
+            CollectionAssert.AreEqual(expecteds, actuals);
+        }
+
+        [TestMethod]
+        public void TestRecursiveBinarySearch()
+        {
+            var items = new int[] { 1, 3, 5, 7, 9, 11 };
+            var value = 5;
+            var expected = 2;
+            var actual = items.RecursiveBinarySearch(value, 0, items.Length);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ DataStructure/Extentions/MyArrayExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure.Extentions
+{
+    public static class MyArrayExtensions
+    {
+
+        public static int IterativeBinarySearch(this int[] items, int value, int start, int end)
+        {
+            while (start <= end)
+            {
+                var index = (start + end) / 2;
+
+                if (items[index] == value)
+                {
+                    return index;
+                }
+                else if (items[index] > value)
+                {
+                    end = index - 1;
+                }
+                else if (items[index] < value)
+                {
+                    start = index + 1;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int RecursiveBinarySearch(this int[] items, int value, int start, int end)
+        {
+            if (start <= end)
+            {
+                var index = (start + end) / 2;
+
+                if (items[index] == value)
+                {
+                    return index;
+                }
+                else if (items[index] > value)
+                {
+                    return RecursiveBinarySearch(items, value, start, end - 1);
+                }
+                else if (items[index] < value)
+                {
+                    return RecursiveBinarySearch(items, value, start + 1, end);
+                }
+            }
+
+            return -1;
+        }
+    }
+}
